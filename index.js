bfba66bfc2c693a2b7daa32b98c03c4dedee4f43
Hilton Pintor <hpbl@cin.ufpe.br>
2018-10-07 17:57:35 -0300

    Fixing response

--- index.js
+++ index.js
@@ -10,52 +10,45 @@
 var request = require('request'); // "Request" library
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
 
 var client_id = 'e81545ea2fac4b499b295ec9f10dc8df'; // Your client id
 var client_secret = 'd04145cf0883444c8a11e6b0cbbcb791'; // Your secret
 
-// your application requests authorization
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .get('/client_credentials', (req, res) => authenticateSpotify(res))
+app.use(express.static(path.join(__dirname, 'public')));
 
-function authenticateSpotify(res) {
+app.get('/', function(req, res) {
+  res.send("Hello World!")
+});
+
+app.get('/client_credentials', function(req, res) {
+
+  // your application requests authorization
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-
-      // use the access token to access the Spotify Web API
-      var token = body.access_token;
-      var options = {
-        url: 'https://api.spotify.com/v1/search',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        json: true,
-        qs: {
-          q: 'tania%20bowra',
-          type: 'artist'
-        }
-      };
-
-      request.get(options, function(error, response, body) {
-        res.send(body);
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
       });
+    } else {
+      console.log("Deu erro")
+      console.log(error)
+      console.log(response)
     }
   });
-};
+});
 
 console.log('Listening on 8888');
 app.listen(8888);
